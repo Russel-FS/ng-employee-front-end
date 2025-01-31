e3a9b781f5e38f1741da5463eba9e28b0bee1ebb
Russel Fs <russelfloressolano900@gmail.com>
2025-01-31 19:46:04 +0000

    integracion de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.ts',
+    './src/**/*.css',
+  ],
+  darkMode: false, 
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
