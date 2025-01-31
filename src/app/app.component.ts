bd08435585838bd42d3c0ee13ea9a1c3579e207d
RUSSEL <russelfloressolano900@gmail.com>
2025-01-30 20:14:46 -0500

    se incluyo servicios empleados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeService } from './services/employee.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'laundry-a';
+  employeeForm!: FormGroup;
+
+  constructor(
+    public fb: FormBuilder,
+    public employeeService: EmployeeService
+  ) {
+
+  }
+  ngOnInit(): void {
+    this.employeeForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+    })
+  }
+  onSubmit() {
+    if (this.employeeForm.valid) {
+      console.log(this.employeeForm.value);
+      alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.employeeForm.value));
+    }
+  }
+
 }
