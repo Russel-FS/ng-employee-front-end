8e60d3c7eef6542d38fcb8a5097faf4594d6279f
Russel Fs <russelfloressolano900@gmail.com>
2025-02-02 01:57:52 +0000

    Cambio de arquitectura a Clean Architecture

--- src/app/core/store/app.state.ts
+++
@@ -1,5 +0,0 @@
-import { EmployeeReducer } from '../../state/reducers/employee-reducer';
-
-export interface AppState {
-    employees: EmployeeReducer;
-}
--- src/app/state/actions/employee.actions.ts
+++
@@ -1,8 +0,0 @@
-import { Employee } from "../../core/models/employee.model";
-import { createAction, props } from "@ngrx/store";
-
-export const getEmployees = createAction('[Employee] Get Employees');
-export const getEmployeesSuccess = createAction(
-    '[Employee] Get Employees success',
-     props<{ employees: Employee[] }>()
-);
